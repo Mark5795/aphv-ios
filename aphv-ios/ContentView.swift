fbb3c90c8b88db56f39d47f24135a8a3f1cc7337
Mark Kea <600331@student.inholland.nl>
2020-11-10 02:25:52 -0800

    init

---
+++ aphv-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  aphv-ios
+//
+//  Created by Mark Kea on 10/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aphv-ios/aphv_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  aphv_iosApp.swift
+//  aphv-ios
+//
+//  Created by Mark Kea on 10/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct aphv_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
